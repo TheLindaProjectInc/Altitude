c3b12cb2eb6e5a09008a7adeb7b1990928926640
nibbles83 <hardke01@yahoo.co.uk>
2022-02-04 13:59:56 +0000

    Fix transaction refreshing on dashboard

--- src/app/metrix/pages/dashboard/dashboard.component.ts
+++ src/app/metrix/pages/dashboard/dashboard.component.ts
@@ -9,12 +9,27 @@ import { WalletService } from '../../providers/wallet.service';
 })
 export class DashboardComponent {
 
+  txTimer;
+  transactions = [];
+  txSub;
+
   constructor(
     public wallet: WalletService,
   ) {
   }
 
+  ngOnInit() {
+    this.transactions = this.wallet.transactions.slice(0, 10);
+    this.txSub = this.wallet.newBlockReceived.subscribe(() => {
+      this.transactions = this.wallet.transactions.slice(0, 10);
+    });
+  }
+
+  ngOnDestroy() {
+    this.txSub.unsubscribe();
+  }
+
   displayTransactions() {
-    return this.wallet.transactions.slice(0, 10);
+    return this.transactions;
   }
 }
