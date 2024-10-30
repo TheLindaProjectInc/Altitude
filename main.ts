637d5c5c1cfab3029db228f00a99a535187af8b9
nibbles83 <hardke01@yahoo.co.uk>
2024-10-30 13:23:11 +0000

    Fix for titlebar style

--- main.ts
+++ main.ts
@@ -46,7 +46,7 @@ function createWindow() {
     icon: path.join(__dirname, 'assets/icons/png/512x512.png'),
     webPreferences: { webSecurity: false, nodeIntegration: true, contextIsolation: false },
     frame: process.platform !== 'win32',
-    titleBarStyle: process.platform === 'linux' ? 'hidden' : 'default'
+    titleBarStyle: process.platform === 'linux' ? 'default' : 'hidden'
   });
 
 
